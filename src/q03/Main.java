d7ce0faf8aa001d87ee7c55b0fa8c020785fb814
Siddhi-209 <csjma21001165697@csjmu.ac.in>
2026-04-08 12:51:08 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -2,19 +2,35 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int[][] matrix = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
 
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
+        int n = matrix.length;
 
+        // Spiral Print
+        int top=0, bottom=n-1, left=0, right=n-1;
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++) System.out.print(matrix[top][i]+" ");
+            top++;
+            for(int i=top;i<=bottom;i++) System.out.print(matrix[i][right]+" ");
+            right--;
+            for(int i=right;i>=left;i--) System.out.print(matrix[bottom][i]+" ");
+            bottom--;
+            for(int i=bottom;i>=top;i--) System.out.print(matrix[i][left]+" ");
+            left++;
+        }
+
+        // Diagonal Sum
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum += matrix[i][i];
+            if(i != n-i-1)
+                sum += matrix[i][n-i-1];
+        }
+
+        System.out.println("\nDiagonal Sum = " + sum);
     }
 }
