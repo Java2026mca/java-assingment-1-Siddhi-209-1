1821e005481c2b34253833a0d28c27e86baae757
Siddhi-209 <csjma21001165697@csjmu.ac.in>
2026-04-08 12:56:43 +0530

    Update Main.java

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -1,15 +1,12 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = 10, a = 0, b = 1;
 
-        // TODO: Print first n Fibonacci numbers separated by spaces
-        //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
-        //
-        // Input: 8
-        // Output: 0 1 1 2 3 5 8 13
-
+        for(int i=1;i<=n;i++){
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
     }
 }
